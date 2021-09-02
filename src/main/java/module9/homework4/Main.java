000ad31d4a0b5a34f52f14e11e252ee0f62f8773
Kirill <schegol.kirill@gmail.com>
2021-09-02 20:49:51 +0300

    module 9 task 4 complete

--- src/main/java/module9/homework4/Main.java
+++ src/main/java/module9/homework4/Main.java
@@ -7,71 +7,45 @@ import org.jsoup.select.Elements;
 
 import javax.swing.*;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        //String htmlFile = parseFile("https://lenta.ru/");
         try {
             File folder = new File("images");
             folder.mkdir();
 
             Document doc = Jsoup.connect("https://lenta.ru/").get();
 
-            Element link = doc.select("a").first();
-            String relHref = link.attr("href"); // == "/"
-            String absHref = link.attr("abs:href"); // "http://jsoup.org/"
-
             Elements elements = doc.select("img");
             elements.forEach(element -> {
-                //System.out.println(element.attr("src"));
-                File image = new File(element.attr("src"));
+                String url1 = element.attr("src");
 
-                //System.out.println(image);
-//                try {
-//                    copyFileUsingChannel(image, folder);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-                try {
-                    Files.copy(Path.of(element.attr("src")), new File("images" + File.separator + image.getName()).toPath());
-                } catch (IOException e) {
-                    e.printStackTrace();
+                Pattern p = Pattern.compile("^https://");
+                Matcher m = p.matcher(url1);
+
+                if(m.find()) {
+                    File image = new File(url1);
+                    try {
+                        URL url = new URL(url1);
+                        InputStream inputStream = url.openStream();
+                        Files.copy(inputStream, new File(folder + "/" + image.getName()).toPath());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             });
-
-            //Elements images = doc.select("")
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
-    private static void copyFileUsingChannel(File source, File dest) throws IOException {
-        FileChannel sourceChannel = null;
-        FileChannel destChannel = null;
-        try {
-            sourceChannel = new FileInputStream(source).getChannel();
-            destChannel = new FileOutputStream(dest).getChannel();
-            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-        }finally{
-            sourceChannel.close();
-            destChannel.close();
-        }
-    }
-//    public static String parseFile(String path) {
-//        StringBuilder sb = new StringBuilder();
-//        try{
-//            List<String> lines = Files.readAllLines(Paths.get(path));
-//            lines.forEach(line -> sb.append(line + "\n"));
-//        }catch (Exception exception){
-//            exception.printStackTrace();
-//        }
-//        return sb.toString();
-//    }
 }
